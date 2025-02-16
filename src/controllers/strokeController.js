68ac0f809e18b3799d2830fa98d1d28c677377c5
abdallateef_shohdy <86281216+abdallateef-sa@users.noreply.github.com>
2025-02-16 20:30:48 +0200

    in this commit , we add AI model

--- src/controllers/strokeController.js
+++ src/controllers/strokeController.js
@@ -1,15 +1,71 @@
-import asyncWrapper from './../middlewares/asyncWrapper.js';
-import ApiError from './../utils/apiError.js';
+import asyncWrapper from "./../middlewares/asyncWrapper.js";
+import ApiError from "./../utils/apiError.js";
+import axios from "axios";
+import multer from "multer";
+import FormData from 'form-data';
 
-export const detection = asyncWrapper(async (req, res) => {
+const BASE_URL = "http://127.0.0.1:8000";
 
-})
-
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 export const prediction = asyncWrapper(async (req, res) => {
+  
+  const response = await axios.post(`${BASE_URL}/predict/`, req.body);
+  
+  res.status(200).json({ status: "success", data: response.data });
+  
+});
 
-})
+export const uploadImage = [
+  upload.single("file"),
+  asyncWrapper(async (req, res) => {
+    
+      if (!req.file) throw new ApiError("No file uploaded", 400);
+
+      const formData = new FormData();
+      formData.append("file", req.file.buffer, {
+        filename: req.file.originalname,
+        contentType: req.file.mimetype,
+      });
+
+      const response = await axios.post(`${BASE_URL}/upload-image/`, formData, {
+        headers: {
+          ...formData.getHeaders(),
+        },
+      });
+
+      res.status(200).json({ status: "success", data: response.data });
+    
+    }
+  ),
+];
 
 export const chatbot = asyncWrapper(async (req, res) => {
+  
+    const response = await axios.post(`${BASE_URL}/chat/`, req.body);
+    res.status(200).json({ status: "success", data: response.data });
 
-})
+});
+
+export const uploadPdf = [
+  upload.single("file"),
+  asyncWrapper(async (req, res) => {
+   
+
+      if (!req.file) throw new ApiError("No file uploaded", 400);
+
+
+      const formData = new FormData();
+      formData.append("file", req.file.buffer, req.file.originalname);
+
+      const response = await axios.post(`${BASE_URL}/upload-pdf/`, formData, {
+        headers: {
+          ...formData.getHeaders(),
+        },
+      });
+
+      res.status(200).json({ status: "success", data: response.data });
+  
+  }),
+];
